48ece1e9c1980137de4d2383e6ee7b10b8cbb604
Marcio Silva <marcioadsworks@gmail.com>
2019-10-22 21:50:19 -0300

    classes Funcionario, Cargo, Endereco e Departamento criadas

---
+++ src/main/java/br/com/secrel/model/Departamento.java
@@ -0,0 +1,12 @@
+package br.com.secrel.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Departamento {
+
+	private Long id;
+	private String nome;
+	
+	private List<Cargo> cargos = new ArrayList<>();
+}
