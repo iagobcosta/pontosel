2a08323c2c56b43743124941fbe424c6faa57c02
Marcio Silva <marcioadsworks@gmail.com>
2019-10-22 22:19:17 -0300

    classes ContraCheque, Gerente, Relatorio e Salario criadas

---
+++ src/main/java/br/com/secrel/model/Gerente.java
@@ -0,0 +1,16 @@
+package br.com.secrel.model;
+
+public class Gerente {
+
+	private Long id;
+	
+	//Metodos
+	
+	public void cadastrarFuncionario() {
+		
+	}
+	
+	public void operation() {
+		
+	}
+}
